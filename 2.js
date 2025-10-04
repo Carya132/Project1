aa1834dcd3abdcd01a5068eec3ebfbf2f6050c99
Charu1325 <charuarya132@gmail.com>
2025-10-04 12:29:19 +0530

    Firstcommit

---
+++ 2.js
@@ -0,0 +1,12 @@
+var a=10;
+var b=20;
+var c=a+b;
+var d=a-b;
+var e=a*b;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+
---
+++ 3.js
@@ -0,0 +1,6 @@
+console.log("hello World!!");
+console.log("Node js!!");
+console.log("Node js is a backend language");
+console.log("Node js is also a server");
+console.log("we ae=re using git hub also");
+console.log("we ae=re using git hub also");
